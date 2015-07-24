f55609de6cc0e1f2483b330975bb6b1af9983779
Javier Villanueva <javier@medialounge.co.uk>
2015-07-24 09:59:59 +0100

    Finish Meeting 2-3 excercise

---
+++ app/code/local/Jvs/Meeting0203/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class Jvs_Meeting0203_IndexController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Display full action name
+     *
+     * @return string
+     */
+    public function indexAction()
+    {
+        echo $this->getFullActionName();
+    }
+}
